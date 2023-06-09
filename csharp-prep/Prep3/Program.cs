ddec289feb35a8b66e5c382b1e1495c2a3616aa0
Miguel Arcos <migark@hotmail.com>
2023-06-09 11:07:40 -0500

    Prep3 task solved

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator  = new Random();
+        int magicNumber = randomGenerator.Next(1,20);
+
+        int guess = -1;
+        int counter = 0;
+
+
+        while (guess != magicNumber)
+        {
+            Console.Write("what is your guess?: ");
+            guess = int.Parse(Console.ReadLine());
+            
+            counter += 1;
+
+            if (magicNumber > guess) 
+            {
+                Console.WriteLine("Magic number is Highest");        
+            }
+            else if (magicNumber < guess)
+            {   
+                Console.WriteLine("Magic Number is Lower");            
+            }
+            else{
+                Console.WriteLine($"You guess it!, you try {counter} times");
+            }
+
+        }
     }
 }
