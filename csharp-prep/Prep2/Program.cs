4ec5f1cfe27fad611be7bebbca2e63b08cea5e57
Miguel Arcos <migark@hotmail.com>
2023-06-09 11:08:59 -0500

    Prep2 task solved

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,27 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.Write("Digit your average: ");
-       int grade = int.Parse(Console.ReadLine());
+          Console.Write("Digit your average: ");
+          int grade = int.Parse(Console.ReadLine());
+          string finalGrade = " ";
+          string last = " ";
+          
+          if (grade <= 100 && grade >= 0) {
 
-       if (grade >= 90)
-       {
-            Console.WriteLine($"Your grade is A");
-       } 
-       else if (grade >= 80) 
-       {
-            Console.WriteLine($"Your grade is B");
-       } 
-       else if (grade >= 70) 
-       {
-            Console.WriteLine($"Your grade is C");
-       } 
-       else if (grade >= 60) 
-       {
-            Console.WriteLine($"Your grade is D");
-       } 
-       else {
-            Console.WriteLine($"Your grade is F");
-       }        
+               if (grade >= 90)
+               {
+                    finalGrade = "A";
+               } 
+               else if (grade >= 80) 
+               {
+                    finalGrade = "B";
+               } 
+               else if (grade >= 70) 
+               {
+                    finalGrade = "C";
+               } 
+               else if (grade >= 60) 
+               {
+                    finalGrade = "D";
+               } 
+               else {
+                    finalGrade = "F";
+               }      
+
+               if (grade < 93 && grade > 61)
+               {
+                    if (grade % 10 > 6)
+                    {
+                         last = "+";
+                    }
+                    else if (grade % 10 < 4)
+                    {
+                         last = "-";
+                    }
+                    else 
+                    {
+                         last = " ";
+                    }
+               }
+               
+               Console.WriteLine($"Your final grade is {finalGrade}{last}");
+          } 
+          else 
+          {
+               Console.WriteLine($"Digited value is invalid");
+          }
     }
 }
