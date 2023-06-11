461721cf5ef706143cbeb202f92ab4cc4fb6d8c0
Miguel Arcos <migark@hotmail.com>
2023-06-11 00:54:20 -0500

    Prep5 worked

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squareAnswer = SquareNumber(userNumber);
+        Console.WriteLine($"The square to {userNumber} is {squareAnswer}");
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+            Console.WriteLine("Welcome to C# ");
+    }
+
+    static string PromptUserName()
+    {
+            Console.Write("Write your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+    }
+
+    static int PromptUserNumber()
+    {
+            Console.Write("Enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+
+        return square;
     }
 }
