f386cf4bb9d72807d0612b311b8f5178e1dd0046
Miguel Arcos <migark@hotmail.com>
2023-06-26 19:06:56 -0500

    W03 read from file complete

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,53 +7,44 @@ class Program
     static void Main(string[] args)
     {
         string usrOption = "";
+        string followOpt = "";
+
         Random randGenerator = new Random();
-        
-        // string scriptureText1 = "Wherefore, whoso believeth in God might with surety hope for a better world, yea, even a place at the right hand of God, which hope cometh of faith, maketh an anchor to the souls of men, which would make them sure and steadfast, always abounding in good works, being led to glorify God.";
-        // Reference newreference1 = new Reference("Ether", 12, 4);
-
-        // string scriptureText2 = "Adam fell that men might be; and men are, that they might have joy.";
-        // Reference newreference2 = new Reference("2 Nefi", 2, 25);
-
-        // string scriptureText3 = "Now the Lord had shown unto me, Abraham, the intelligences that were organized before the world was; and among all these there were many of the noble and great ones; Abraham, thou art one of them; thou wast chosen before thou wast born.";
-        // Reference newreference3 = new Reference("Abraham",3,22,23);
-
-        // Scripture newScripture = new Scripture(newreference1, scriptureText1);
-        // Scripture newScripture2 = new Scripture(newreference2, scriptureText2);
-        // Scripture newScripture3 = new Scripture(newreference3, scriptureText3);
 
         List<Scripture> gems = ReadFromFile();
-        int gemIndex = randGenerator.Next(0,gems.Count()-1);
 
-        
-        // gems.Add(newScripture);
-        // gems.Add(newScripture2);
-        // gems.Add(newScripture3);
-
-        // foreach (Scripture gem in gems)
-        // {
-        //     Console.WriteLine(gem.GetDisplayText() +"\n");
-        // }
-
-        while (usrOption != "quit")
+        while (followOpt != "n")
         {
-            
-            
-            if (usrOption == "" && gems[gemIndex].AllHidden() == false) // and newScripture.IsCompletelyHidden == false
+
+            int gemIndex = randGenerator.Next(0,gems.Count()-1);
+
+            while (usrOption != "quit")
             {
-                Console.WriteLine(gems[gemIndex].GetDisplayText() +"\n");
-                Console.Write("\n" + "[ENTER] to continue or type 'quit' to finish:  ");
-                usrOption = Console.ReadLine();
-                gems[gemIndex].HideRandomWords();
-                Console.Clear();
+                if (usrOption == "" && gems[gemIndex].AllHidden() == false) // and newScripture.IsCompletelyHidden == false
+                {
+                    Console.WriteLine(gems[gemIndex].GetDisplayText() +"\n");
+                    Console.Write("\n" + "[ENTER] to continue or type 'quit' to finish:  ");
+                    usrOption = Console.ReadLine();
+                    gems[gemIndex].HideRandomWords();
+                    Console.Clear();
+                }
+                else 
+                {
+                    Console.WriteLine(gems[gemIndex].GetDisplayText() +"\n");
+                    usrOption = "quit";
+                }
             }
-            else 
+            Console.Write("\nDo you want to try with other scripture? [y]/[n] ");
+            followOpt = Console.ReadLine();
+
+            if (followOpt == "y")
             {
-                Console.WriteLine(gems[gemIndex].GetDisplayText() +"\n");
-                usrOption = "quit";
-                Console.WriteLine("\nSee you the next time! ");
+                Console.Clear();
+                usrOption = "";
             }
         }
+
+        Console.WriteLine("\nSee you the next time! ");
     }
 
     public static List<Scripture> ReadFromFile()
@@ -91,3 +82,22 @@ class Program
         return gems;
     }
 }
+
+        // string scriptureText1 = "Wherefore, whoso believeth in God might with surety hope for a better world, yea, even a place at the right hand of God, which hope cometh of faith, maketh an anchor to the souls of men, which would make them sure and steadfast, always abounding in good works, being led to glorify God.";
+        // Reference newreference1 = new Reference("Ether", 12, 4);
+        // string scriptureText2 = "Adam fell that men might be; and men are, that they might have joy.";
+        // Reference newreference2 = new Reference("2 Nefi", 2, 25);
+        // string scriptureText3 = "Now the Lord had shown unto me, Abraham, the intelligences that were organized before the world was; and among all these there were many of the noble and great ones; Abraham, thou art one of them; thou wast chosen before thou wast born.";
+        // Reference newreference3 = new Reference("Abraham",3,22,23);
+        // Scripture newScripture = new Scripture(newreference1, scriptureText1);
+        // Scripture newScripture2 = new Scripture(newreference2, scriptureText2);
+        // Scripture newScripture3 = new Scripture(newreference3, scriptureText3);
+
+        // gems.Add(newScripture);
+        // gems.Add(newScripture2);
+        // gems.Add(newScripture3);
+
+        // foreach (Scripture gem in gems)
+        // {
+        //     Console.WriteLine(gem.GetDisplayText() +"\n");
+        // }        
