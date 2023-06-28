382218c57a4b930154692641b092108bc0fba5cd
Miguel Arcos <migark@hotmail.com>
2023-06-28 04:50:24 -0500

    Learning04 complete

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -2,7 +2,7 @@ public class MathAssignment: Assignment
 {
     private string _textbookSection, _problems;
 
-    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    public MathAssignment(string studentName, string topic, string textbookSection = "", string problems = "") : base(studentName, topic)
     {
         _textbookSection = textbookSection;
         _problems = problems;
